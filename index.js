31327720652692cbec181564657e992cbc8952a0
imani <18imani.vata@gmail.com>
2024-04-05 15:35:01 +0300

    done

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((sentence) => {
+    return sentence
+      .split(" ")
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  });
+};
+
